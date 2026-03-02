db16ed00b4479259f5bf25309ac6ddad06f578bb
XMG-DEMIOS\davis <davis.stoewer@gmail.com>
2026-03-02 10:38:24 +0100

    First draft of task 2 implemented

--- task1_and_2.py
+++ task1_and_2.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 from turbine_blade import TurbineBlade
 from direct_stiffness import Frame, BeamElement, ManualCrossSection
+import classical_laminate_theory as clt
 
 # %% Create blade from input data
 data_dir = Path(__file__).parent / "_data"
@@ -31,7 +32,7 @@ d = t_bld+t_spar
 I_bld = t_spar * w_spar * d**2 / 2
 EI_bld = E_f * I_bld
 
-# %% Calculate the bending
+# %% Solve the beam for the given loads
 # Set up the frame system
 profiles = [ManualCrossSection(A=2*w_spar[i]*d[i],  iy=np.inf, iz=I_bld[i],
                                j=np.inf)
@@ -59,8 +60,10 @@ fixed_dofs = ("u_x1", "u_z1", "theta_y1")
 # Solve the frame system
 U, R = frame.solve_fea_system(F=F.flatten(), fixed_dofs=fixed_dofs,
                               is_3d=False)
-U = U.reshape((frame.n_nodes, 3)) #TODO: include tilt and precone
+U = U.reshape((frame.n_nodes, 3))
+R = R.reshape((frame.n_nodes, 3))
 
+# %% Task 1: Tower clearance
 U_tip = U[-1, :]
 print(f"Tip deflection {U_tip[1]:.2f} m")
 
@@ -79,6 +82,71 @@ x = np.zeros(len(U))
 x[:-1] = span_coords[:, 0]
 x[-1] = span_coords[-1, 1]
 ax.plot(x, U[:, 1])
-ax.set_xlabel("x [m]")
-ax.set_ylabel("u [m]")
+ax.set_xlabel(r"$x$ [m]")
+ax.set_ylabel(r"$u$ [m]")
 ax.grid()
+
+# %% Task 2: Buckling and strength
+# %%% Task 2a: Buckling
+# Calculate cross sectional stress from beam bending
+udd = np.gradient(np.gradient(U[:, 1], x), x)
+sigma = np.zeros((frame.n_nodes, 2))
+sigma[:-1, 0] = udd[:-1] * E_f * (t_bld/2+t_spar)
+sigma[1:, 1] = udd[1:] * E_f * (t_bld/2+t_spar)
+
+idx_crit = np.argmax(sigma, axis=1)
+
+# Calculate ABD matrix for cross sectios
+ABD_bld = np.stack([section.laminate.ABD_matrix for section in laminates])
+D_bld = ABD_bld[:, 3:6, 3:6]
+
+# Calculate critical buckling load
+def P_func(m, a, b, D):
+    return np.pi**2 * (D[...,0,0]*(m/a)**2
+                       + 2*(D[...,0,1] + 2*D[:,2,2])*(1/b)**2
+                       + D[...,1,1]*(a/m)**2*(1/b)**4)
+
+l_sections = np.array([section.length for section in laminates])
+P_buckling = np.stack([P_func(m=m+1, a=l_sections, b=w_spar, D=D_bld)
+                      for m in range(10)],
+                      axis = 0)
+
+P_cr_buckling = np.min(P_buckling, axis=0)
+
+fig, ax = plt.subplots(figsize=(16, 10))
+ax.plot(x, sigma)
+ax.set_xlabel(r"$x$ [m]")
+ax.set_ylabel(r"$\sigma$ [N/m^2]")
+ax.grid()
+
+fig, ax = plt.subplots(figsize=(16, 10))
+ax.plot(span_coords[:, 0], P_cr_buckling)
+ax.set_xlabel(r"$x$ [m]")
+ax.set_ylabel(r"$P_{cr}$ [N]")
+ax.grid()
+
+# %%% Task 2b: Strength
+sigma1_max = np.max([sigma[:-1, 0], sigma[1:, 1]], axis=0)
+
+failure_tsaihill = np.zeros(len(laminates))
+failure_tsaiwu = np.zeros(len(laminates))
+
+for i in range(len(laminates)):
+    failure_tsaihill[i] = \
+        np.max([
+            clt.failure.TsaiHill.failure_index(
+                stress=(sigma1_max[i], 0, 0),
+                **laminates[i].laminate.plies[j].material.strength_as_dict())
+            for j in range(len(laminates[i].laminate.plies))]
+            )
+
+    failure_tsaiwu[i] = \
+        np.max([
+            clt.failure.TsaiWu.failure_index(
+                stress=(sigma1_max[i], 0, 0),
+                **laminates[i].laminate.plies[j].material.strength_as_dict())
+            for j in range(len(laminates[i].laminate.plies))]
+            )
+
+if any(failure_tsaiwu>=1) or any(failure_tsaihill>=1):
+    print("Failure detected.")
